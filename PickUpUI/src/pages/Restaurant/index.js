8fe6f1a7db7bc2cd0602037d7cc55918ca71235f
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-30 16:10:15 -0400

    add pages for Orders and Restaurant to render

---
+++ PickUpUI/src/pages/Restaurant/index.js
@@ -0,0 +1,10 @@
+import React from 'react' 
+import Restaurant from '../../components/Restaurant'
+
+export const restaurant = () => {
+    return (
+        <div>
+            <Restaurant/>
+        </div>
+    )
+}
