1a3afcc06857b6ccd6b53487e8248f05a4cf0ad8
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-30 12:13:12 -0400

    add footer component

--- PickUpUI/src/App.js
+++ PickUpUI/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { RouterConfig } from './navigation/RouterConfig';
 import {Navigation} from "./components/Navigation"
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-       <Navigation/>
-       <RouterConfig/>
-      
+      <Navigation/>
+      <RouterConfig/>
+      <Footer />
      
     </div>
   );
