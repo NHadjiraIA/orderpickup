27f660599aa7f800c35cffc3eaef7c39b3ea21a1
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-30 18:38:21 -0400

    add path for carts

--- PickUpUI/src/navigation/CONSTANTS.js
+++ PickUpUI/src/navigation/CONSTANTS.js
@@ -4,3 +4,4 @@ export const DISH = "/alldishs";
 export const RESTAURANT = '/restaurant';
 export const COMMENTS = '/restaurant/comments';
 export const ORDERS = '/orders';
+export const CART = '/cart';
