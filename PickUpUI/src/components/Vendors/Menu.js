cf96ce25fee990b705c782531f039da43413944c
Shivani <shvni.here@gmail.com>
2021-08-31 19:30:03 -0400

    Modifying menu page

--- PickUpUI/src/components/Vendors/Menu.js
+++ PickUpUI/src/components/Vendors/Menu.js
@@ -1,14 +1,103 @@
 import { Typography } from '@material-ui/core';
-import React from 'react'; 
 
-function Menu() {
-  // const classes = useStyles();
+
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { Button } from '@material-ui/core';
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+function createData(img, name, calories, desc, size,type,price) {
+  return { img, name, calories, desc, size,type,price };
+}
+
+const rows = [
+  createData("https://images.unsplash.com/photo-1543826173-70651703c5a4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=799&q=80",'Frozen yoghurt', 159,6,0,  <Button variant="contained" color="primary" disableElevation>
+  DELETE
+</Button>, 4.0,),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+
+  productImg: {
+    height: '70%',
+    width: '10%'
+  },
+});
+
+ function Menu() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <Typography>Menu</Typography>
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Image</StyledTableCell>
+            <StyledTableCell align="right">Item name</StyledTableCell>
+            <StyledTableCell align="right">Calories&nbsp;(g)</StyledTableCell>
+            <StyledTableCell align="right">Description</StyledTableCell>
+            <StyledTableCell align="right">Size&nbsp;</StyledTableCell>
+            <StyledTableCell align="right">Type&nbsp;</StyledTableCell>
+            <StyledTableCell align="right">Price&nbsp;</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+              <img className={classes.productImg} src={row.img}/>
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.name}</StyledTableCell>
+              <StyledTableCell align="right">{row.calories}</StyledTableCell>
+              <StyledTableCell align="right">{row.description}</StyledTableCell>
+              <StyledTableCell align="right">{row.size}</StyledTableCell>
+              <StyledTableCell align="right">{row.type}</StyledTableCell>
+              <StyledTableCell align="right">{row.price}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+// function Menu() {
+//   // const classes = useStyles();
+//   return (
+//     <div>
+//       <Typography>Menu</Typography>
       
-    </div>
-  )
-};
+//     </div>
+//   )
+// };
 
 export default Menu;
