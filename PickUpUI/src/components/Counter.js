a5155846b5f31498606913e4e7c104b316fed8a0
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-31 14:02:24 -0400

    implement counter and add to productListItem

--- PickUpUI/src/components/Counter.js
+++ PickUpUI/src/components/Counter.js
@@ -1,25 +1,51 @@
-import React, { useEffect, useState } from "react"; 
+import React, { Component } from "react"; 
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
 import IconButton from '@material-ui/core/IconButton';
 
-const Counter = () => {
 
-  
+class Counter extends Component {
 
-  return (
-    <>
-      <IconButton 
-      aria-label="minus-item" 
-      >
-        <RemoveCircleIcon fontSize='large'/>
-      </IconButton>
-      <IconButton aria-label="add-item" >
-        <AddCircleIcon fontSize='large'/>
-      </IconButton>
-    </>
-  );
-};
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
 
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  decrement = () => {
+    if (this.state.count > 0) {
+      this.setState({
+        count: this.state.count - 1
+      })
+    }
+  }
+
+  render () {
+    return (
+      <div>
+
+        <IconButton 
+          aria-label="minus-item" 
+          onClick={this.decrement}
+        >
+          <RemoveCircleIcon fontSize='large'/>
+        </IconButton>
+        {this.state.count}
+        <IconButton 
+          aria-label="add-item" 
+          onClick={this.increment}
+        >
+          <AddCircleIcon fontSize='large'/>
+        </IconButton>
+      </div>
+    )
+  }
+}
 export default Counter;
-                
