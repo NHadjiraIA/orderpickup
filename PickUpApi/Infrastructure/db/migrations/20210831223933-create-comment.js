1a38c8b41004a78c4f6e3223fd76fcfbd00da9dc
Hadjira <bkh.hadjira@gmail.com>
2021-08-31 19:50:29 -0400

    - Added models (entitites) - Added migrations Next step: adding endpoints / repositories / dtos /mappers /

---
+++ PickUpApi/Infrastructure/db/migrations/20210831223933-create-comment.js
@@ -0,0 +1,37 @@
+'use strict';
+const sequelize = require("sequelize");
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: sequelize.INTEGER,
+        allowNull: false,
+      },
+      restaurantId: {
+        type: sequelize.INTEGER,
+        allowNull: false,
+      },
+      Comment: {
+        type: sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('comments');
+  }
+};
