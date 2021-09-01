2ccfeb44092071d90cc2438326277737f4f0c7e7
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-09-01 19:59:14 -0400

    fix bug on nav bar

--- PickUpUI/src/App.js
+++ PickUpUI/src/App.js
@@ -47,7 +47,7 @@ const restaurantsList = [
 
 function App() {
   return (
-    <div className="App" style={{minHeight: '100vh', display: 'flex', flexDirection: 'column'}}>
+    <div className="App" >
       <Navigation/>
       <RouterConfig/>
       <Footer />
