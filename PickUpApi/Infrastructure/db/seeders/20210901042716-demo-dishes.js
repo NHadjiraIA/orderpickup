d8e9761e85761010de5deb559cf3445c9d36d3cd
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-09-01 23:22:40 -0400

    added more data to dishes

--- PickUpApi/Infrastructure/db/seeders/20210901042716-demo-dishes.js
+++ PickUpApi/Infrastructure/db/seeders/20210901042716-demo-dishes.js
@@ -30,7 +30,48 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date()
      }]);
-  
+
+
+      await queryInterface.bulkInsert('dishes', [{
+       restaurantId: 1,
+      name: "Strawberry pie",
+      img_url: "https://images.unsplash.com/photo-1585034722868-115328443114?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80",
+      price: 8.50,
+      calories: 490,
+      description:"Filled with huge, sweet strawberries bursting with juice and baked in  the most perfect, flaky, cannabis infused buttery pie crust",
+      size: "Medium",
+      vegan: false,
+      gluten: true,
+      halal: false,
+      dairy: false,
+      nuts: true,
+      available: true,
+      marijuana: true,
+      type: "Dessert",
+       createdAt: new Date(),
+       updatedAt: new Date()
+      }]);
+      
+      await queryInterface.bulkInsert('dishes', [{
+        restaurantId: 1,
+       name: "Chocolate Cake",
+       img_url: "https://images.unsplash.com/photo-1617455559706-fa196228c05d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+       price: 12.50,
+       calories: 610,
+       description:"This cake is moist and has the perfect crumb. It has cocoa from Costa Rica, hand harvested by children.",
+       size: "Medium",
+       vegan: false,
+       gluten: true,
+       halal: false,
+       dairy: false,
+       nuts: true,
+       available: true,
+       marijuana: true,
+       type: "Dessert",
+        createdAt: new Date(),
+        updatedAt: new Date()
+       }]);
+     
   },
 
   down: async (queryInterface, Sequelize) => {
