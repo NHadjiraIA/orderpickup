7b2cabe942b0f234cde0c25a981161595e69659f
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-09-03 01:00:40 -0400

    add twilio promise to code, still need to private the account data

--- PickUpApi/orders.ts
+++ PickUpApi/orders.ts
@@ -5,9 +5,14 @@ import {toEntity as orderDetailToEntity} from './application/mappers/orderDetail
 import { OrderDetailsRepository } from "./Infrastructure/repositories/OrderDetailsRepository";
 import { OrderDto } from "./domain/dtos/OrderDto";
 import { OrderDetailDto } from "./domain/dtos/OrderDetailDto";
-import {orderReceivedNotice} from './twilio'
 
 export class OrdersApi{
+    accountSid = process.env.TWILIO_ACCOUNT_SID;
+    authToken = process.env.TWILIO_AUTH_TOKEN;
+    client = require("twilio")(this.accountSid, this.authToken);
+    myNumber = "+16478719181";
+    twilioNumber = "+12393120406";
+
     private _ordersRepository:any;
     private _orderDetailsRepository:any;
     constructor(){
@@ -33,7 +38,15 @@ export class OrdersApi{
             orderDto.Details.forEach(detail => {
                 this._orderDetailsRepository.Create(orderDetailToEntity(detail, createdOrder.id));
             });
-            orderReceivedNotice();
+            this.client.messages
+                .create({
+                  body: "Your order has been received, we are working on it now!",
+                  from: this.twilioNumber,
+                  to: this.myNumber,
+                })
+                .then((message) => console.log("MESSAGE", message))
+                .catch((error) => console.log(error))
+                .done();
             return res.status(201).json(createdOrder);
         }else{
             return res.status(400).send("The order could not be created. Please check the provided data.")
