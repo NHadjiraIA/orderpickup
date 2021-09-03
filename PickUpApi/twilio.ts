33063a232faebdd9b41c2694cd5541caa3061305
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-09-03 01:43:01 -0400

    comment out unnecessary code

--- PickUpApi/twilio.ts
+++ PickUpApi/twilio.ts
@@ -8,38 +8,38 @@
 
 // const accountSid = process.env.TWILIO_ACCOUNT_SID;
 // const authToken = process.env.TWILIO_AUTH_TOKEN;
-const accountSid = "ACcbeb0d2e1ca0ce0a2745ee0bfe376703";
-const authToken = "585a5fb0eacf2b3d133676fe5dc2a2d5";
+export const accountSid = "ACcbeb0d2e1ca0ce0a2745ee0bfe376703";
+export const authToken = "585a5fb0eacf2b3d133676fe5dc2a2d5";
 
-const client = require("twilio")(accountSid, authToken);
+export const client = require("twilio")(accountSid, authToken);
 
-const myNumber = "+16478719181";
-const twilioNumber = "+12393120406";
+export const myNumber = "+16478719181";
+export const twilioNumber = "+12393120406";
 
 //put vars in .env
 
-export const pickUpNotice = () => {
-  client.messages
-    .create({
-      body: "Your food is ready for pickup.",
-      from: twilioNumber,
-      to: myNumber,
-    })
-    .then((message) => console.log("MESSAGE", message))
-    .catch((error) => console.log(error))
-    .done();
-};
+// export const pickUpNotice = () => {
+//   client.messages
+//     .create({
+//       body: "Your food is ready for pickup.",
+//       from: twilioNumber,
+//       to: myNumber,
+//     })
+//     .then((message) => console.log("MESSAGE", message))
+//     .catch((error) => console.log(error))
+//     .done();
+// };
 
-export const orderReceivedNotice = () => {
-  client.messages
-    .create({
-      body: "Thank you for your payment. Your order has been received, we are working on it now!",
-      from: twilioNumber,
-      to: myNumber,
-    })
-    .then((message) => console.log("MESSAGE", message))
-    .catch((error) => console.log(error))
-    .done();
-};
+// export const orderReceivedNotice = () => {
+//   client.messages
+//     .create({
+//       body: "Thank you for your payment. Your order has been received, we are working on it now!",
+//       from: twilioNumber,
+//       to: myNumber,
+//     })
+//     .then((message) => console.log("MESSAGE", message))
+//     .catch((error) => console.log(error))
+//     .done();
+// };
 
 // export default { orderReceivedNotice, pickUpNotice };
