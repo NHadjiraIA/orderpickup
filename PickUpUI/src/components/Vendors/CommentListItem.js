6405f6a9827d83706a29e3a1b6d74ada132f0930
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-09-05 01:30:29 -0400

    left align on vendor comments

--- PickUpUI/src/components/Vendors/CommentListItem.js
+++ PickUpUI/src/components/Vendors/CommentListItem.js
@@ -1,90 +1,74 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 import { Grid, Paper, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  typography: {
-    // width: '100px',
-  },
-
   commentPage: {
-    padding: '10%', 
-    display: 'flex',
-    flexDirection: 'row',
-    marginLeft: '4em',
-    marginRight: '4em',
+    padding: "10%",
+    display: "flex",
+    flexDirection: "row",
+    marginLeft: "4em",
+    marginRight: "4em",
   },
 
   filters: {
-    width: '40%',
-    height: '30%',
-    backgroundColor: '#e9ebf0',
-    marginRight: '1.5em',
+    width: "40%",
+    height: "30%",
+    backgroundColor: "#e9ebf0",
+    marginRight: "1.5em",
   },
 
   postAndPostedComments: {
-    flexDirection: 'column',
-    width: '60%', 
-
+    flexDirection: "column",
+    width: "60%",
   },
 
   postComment: {
-    backgroundColor: '#e9ebf0',
-    padding: '1em',
+    backgroundColor: "#e9ebf0",
+    padding: "1em",
   },
 
   commentForm: {
-    display: 'flex',
-    flexDirection: 'column',
-    
+    display: "flex",
+    flexDirection: "column",
   },
   commentText: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap",
   },
 
   postButton: {
-    marginTop: '1em',
-    backgroundColor: 'black',
-    color: 'white',
-    width: '20%',
-  }
-
-
+    marginTop: "1em",
+    backgroundColor: "black",
+    color: "white",
+    width: "20%",
+  },
 }));
 
 function CommentListItem(props) {
   const classes = useStyles();
 
   return (
-
     <Paper style={{ padding: "40px 20px", marginTop: 100 }}>
       <Grid container wrap="nowrap" spacing={2}>
         {/* <Grid item> */}
-          {/* <Avatar alt="Remy Sharp" src={imgLink} /> */}
+        {/* <Avatar alt="Remy Sharp" src={imgLink} /> */}
         {/* </Grid> */}
         <Grid justifyContent="left" item xs zeroMinWidth>
           <h4 style={{ margin: 0, textAlign: "left" }}>{props.username}</h4>
           <br />
           {/* <div className={classes.commentText}> */}
-            <Typography className={classes.typography}>
-              {props.comment}
-              {" "}
-            </Typography>
+          <Typography align="left">{props.comment} </Typography>
           {/* </div> */}
 
           <br />
           <p style={{ textAlign: "left", color: "gray" }}>
-            posted 1 minute ago
+            {/* posted 1 minute ago */}
           </p>
         </Grid>
       </Grid>
     </Paper>
-    
-    
-    
   );
 }
-
 
 export default CommentListItem;
